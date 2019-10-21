8d6e6186a92e664d73d20d6d1ee3ee5f03fa47b4
nonylene <nonylene@gmail.com>
2019-10-21 18:17:51 +0900

    Add --help, --version
    Closes https://github.com/nonylene/yaml2json/issues/2

--- src/yaml2json.nim
+++ src/yaml2json.nim
@@ -2,18 +2,30 @@ import streams
 import yaml
 import json
 import logging
-import os
+import parseopt
+import parseopt
+import ./meta
+
+const USAGE_PROMPT = "Usage: yaml2json [-h,-v] [filename or - (STDIN)]"
+const VERSION_PROMPT = "yaml2json " & meta.VERSION
 
 var logger = newConsoleLogger()
 addHandler(logger)
 
-proc main() =
-  if paramCount() != 1:
-    error("usage: yaml2json [filename or -]")
-    quit(1)
+proc writeHelp() =
+  echo VERSION_PROMPT
+  echo USAGE_PROMPT
+  quit()
 
-  let filename = paramStr(1)
+proc errorHelp() =
+  error(USAGE_PROMPT)
+  quit(1)
 
+proc writeVersion() =
+  echo VERSION_PROMPT
+  quit()
+
+proc main(filename: string) =
   var file: File
   defer: file.close()
   if filename == "-":
@@ -26,4 +38,22 @@ proc main() =
   for node in l:
     echo pretty(node)
 
-main()
+# parseopt
+var filename: string
+var parser = initOptParser(shortNoVal = {'h'}, longNoVal = @["help"])
+
+for kind, key, val in parser.getopt():
+  case kind
+  of cmdArgument:
+    filename = key
+  of cmdLongOption, cmdShortOption:
+    case key
+    of "help", "h": writeHelp()
+    of "version", "v": writeVersion()
+  of cmdEnd: assert(false) # This should not occur
+
+if filename == "":
+  # argument not passed
+  errorHelp()
+
+main(filename)
--- yaml2json.nimble
+++ yaml2json.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "0.1.1"
+version       = "0.2.0" ## variable cannot be used since nimble caches .nimble file.
 author        = "nonylene"
 description   = "yaml2json"
 license       = "MIT"
